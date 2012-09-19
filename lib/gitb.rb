a0cefcacaa2deb6effbf0caa93d4501b846ac46c
Matt Raibert <mattraibert@gmail.com>
2012-09-19 13:59:45 -0400

    dry up gitb (reuse git_branch code)

--- lib/gitb.rb
+++ lib/gitb.rb
@@ -1,20 +1,4 @@
-
-RED="\033[0;31m"
-YELLOW="\033[0;33m"
-GREEN="\033[0;32m"
-NO_COLOR="\033[0m"
-
-def red s
-  "#{RED}#{s}#{NO_COLOR}"
-end
-
-def green s
-  "#{GREEN}#{s}#{NO_COLOR}"
-end
-
-def yellow s
-  "#{YELLOW}#{s}#{NO_COLOR}"
-end
+require 'git_branch'
 
 def project_dirs
   Dir["**/.git"].map {|git_dir| File.dirname(git_dir) }
@@ -22,21 +6,9 @@ end
 
 def do_branch
   outputs = project_dirs.map do |project_dir|
-    status = ""
     Dir.chdir(project_dir) do
       "#{project_dir}: #{apply_status branch}"
     end
   end
   puts outputs.join "\n"
-end
-
-def apply_status branch
-  status = `git status --porcelain`
-  return red "<#{branch}>" if /^ ([MARCD])/ =~ status
-  return yellow "{#{branch}}" if /^([MARCD]) / =~ status
-  return green "(#{branch})"
-end
-
-def branch
-  `git branch`.lines.select {|line| /^\*/ =~ line }.first.sub(/^\* /,"").chomp
 end
