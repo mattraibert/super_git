4341e7f2d40df410c390d8e3be6b5bf6a6507695
Matt Raibert <mattraibert@gmail.com>
2012-09-24 18:59:22 -0400

    fix up

--- lib/gitall.rb
+++ lib/gitall.rb
@@ -2,9 +2,11 @@ require 'gits'
 
 def do_this_to_all_the_gits
   outputs = project_dirs.map do |project_dir|
+    status = ""
     Dir.chdir(project_dir) do
-      status = yield(project_dir)
+      status = yield(project_dir).chomp
     end
+    status
   end
   puts outputs.join "\n"
 end
