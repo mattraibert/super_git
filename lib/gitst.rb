e309eb93c4b8907edb8297b0aa168ca1d8db11b4
Matt J Raibert <cyrus@staple>
2013-05-29 19:21:07 -0400

    add colors to gitst

--- lib/gitst.rb
+++ lib/gitst.rb
@@ -1,7 +1,9 @@
+require 'color'
+
 def pretty_status
   status = `git status --porcelain`
-  status.gsub!(/^([MARCD]) /, "    {stage}      \\1")
-  status.gsub!(/^[MARCD ]([MARCD])/, " <working copy>  \\1")
+  status.gsub!(/^([MARCD]) /, Color.yellow("    {stage}      \\1"))
+  status.gsub!(/^[MARCD ]([MARCD])/, Color.red(" <working copy>  \\1"))
   status.gsub!(/^\?/, "    **new**      ")
   "\n#{status}"
 end
