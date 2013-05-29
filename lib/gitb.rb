555cff3e369ccbab291877671ed974cb306a349a
Matt J Raibert <cyrus@staple>
2013-05-29 19:05:54 -0400

    simplify gitall calls

--- lib/gitb.rb
+++ lib/gitb.rb
@@ -1,9 +1,6 @@
 require 'git_branch'
-require 'gits'
 require 'gitall'
 
 def do_branch
-  do_this_to_all_the_gits do |project_dir|
-      "#{apply_status branch}"
-  end
+  do_this_to_all_the_gits { "#{apply_status branch}" }
 end
--- lib/gitst.rb
+++ lib/gitst.rb
@@ -1,12 +1,14 @@
 require 'gits'
 require 'gitall'
 
+def pretty_status
+  status = `git status --porcelain`
+  status.gsub!(/^([MARCD]) /, "    {stage}      \\1")
+  status.gsub!(/^[MARCD ]([MARCD])/, " <working copy>  \\1")
+  status.gsub!(/^\?/, "    **new**      ")
+  "\n#{status}"
+end
+
 def do_status
-  do_this_to_all_the_gits do |project_dir|
-    status = `git status --porcelain`
-    status.gsub!(/^([MARCD]) /, "    {stage}      \\1")
-    status.gsub!(/^[MARCD ]([MARCD])/, " <working copy>  \\1")
-    status.gsub!(/^\?/,         "    **new**      ")
-    "\n#{status}"
-  end
+  do_this_to_all_the_gits { pretty_status }
 end
