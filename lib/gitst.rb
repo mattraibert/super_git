bf825f62ec61c1ce9a90d4967e270099c48d88ac
Matt J Raibert <cyrus@staple>
2013-05-29 18:49:38 -0400

    fix a bug when files are partially staged

--- lib/gitst.rb
+++ lib/gitst.rb
@@ -5,7 +5,7 @@ def do_status
   do_this_to_all_the_gits do |project_dir|
     status = `git status --porcelain`
     status.gsub!(/^([MARCD]) /, "    {stage}      \\1")
-    status.gsub!(/^ ([MARCD])/, " <working copy>  \\1")
+    status.gsub!(/^[MARCD ]([MARCD])/, " <working copy>  \\1")
     status.gsub!(/^\?/,         "    **new**      ")
     "#{' '*16} #{branch_local}#{status}"
     "#{'#'*16} #{project_dir} #{'#'*16}\n#{status}"
