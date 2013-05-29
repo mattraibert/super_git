7b6f683f9a42b04093e421ec3772ab7401dbad16
Matt J Raibert <cyrus@staple>
2013-05-29 19:08:14 -0400

    move multi-git feature out of gitst.rb

--- lib/gitst.rb
+++ lib/gitst.rb
@@ -1,14 +1,7 @@
-require 'gits'
-require 'gitall'
-
 def pretty_status
   status = `git status --porcelain`
   status.gsub!(/^([MARCD]) /, "    {stage}      \\1")
   status.gsub!(/^[MARCD ]([MARCD])/, " <working copy>  \\1")
   status.gsub!(/^\?/, "    **new**      ")
   "\n#{status}"
-end
-
-def do_status
-  do_this_to_all_the_gits { pretty_status }
 end
